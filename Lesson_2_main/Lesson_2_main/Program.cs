6c804b2a11930fdca2b133989c3827849d31d1ce
aNDiQ-CS <74787127+aNDiQ-CS@users.noreply.github.com>
2025-10-26 02:42:00 +0300

    Add solution

---
+++ Lesson_2_main/Lesson_2_main/Program.cs
@@ -0,0 +1,115 @@
+﻿using aNDiQLib;
+using System;
+
+namespace Lesson_2_main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> a = new MyList<int>();
+            Console.WriteLine("MyList from aNDiQLib");
+            Console.WriteLine("------");
+            Console.WriteLine("\nFilling MyList with 10 digits");
+
+            for (int i = 0; i < 10; i++)
+            {
+                a.Add(i);
+                Console.WriteLine(a);   
+                Thread.Sleep(400);
+            }
+            Console.WriteLine("\nMyList after adding 10 digits");
+            Console.WriteLine(a);
+
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("\nInserting and removing some elements");
+            a.Insert(1337, 0);
+            Console.WriteLine("Insert 1337 to index 0: " + a);
+            a.Insert(52, 1);
+            Console.WriteLine("Insert 52 to index 1: " + a);
+            a.Insert(123, 9);
+            Console.WriteLine("Insert 123 to index 9: " + a);
+            a.Remove(123);
+            Console.WriteLine("Remove number 123: " + a);
+            a.Remove(1);
+            Console.WriteLine("Remove number 1: " + a);
+            a.RemoveAt(10);
+            Console.WriteLine("Remove number from index 10: " + a);
+            a.RemoveAt(1);
+            Console.WriteLine("Remove number from index: " + a);
+            a.RemoveAt(1);
+            Console.WriteLine("Remove number from index 10: " + a);
+            a.RemoveAt(1);
+            Console.WriteLine("Remove number from index 10: " + a);
+            a.Remove(a[2]);
+            Console.WriteLine("Remove number from a[2]: " + a);
+            a[0] = 228;
+            Console.WriteLine("a[0] = 228: " + a);            
+
+            Console.WriteLine("\nLet's check, if MyArray contains your number!");
+            Console.Write("Enter your number: ");            
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (a.Contains(number))
+                {
+                    Console.WriteLine("Yep, we have it!");
+                }
+                else
+                {
+                    Console.WriteLine("Nope, there's nothing");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that's not a number");
+            }
+
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE...");
+            Console.ReadKey();
+
+            Console.WriteLine("\nClearing MyList");
+            a.Clear();
+            Console.WriteLine(a);
+
+            Console.WriteLine("Now, we'll fill MyList with 33 digits to test for the last time");
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE...");
+            Console.ReadKey();            
+            Console.Clear();
+            for (int i = 0; i < 33; i++)
+            {
+                a.Add(i);
+                Console.WriteLine(a);
+                Thread.Sleep(250);
+            }            
+
+            Console.WriteLine("\nNow, we'll delete MyList item by item");
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE...");
+            Console.ReadKey();
+            Console.Clear();
+
+            for (int i = 0; i < 33; i++)
+            {
+                a.RemoveAt(0);
+                Console.WriteLine(a);
+                Thread.Sleep(250);
+            }
+            Console.WriteLine(a);
+
+            Console.WriteLine("Now, we'll fill MyList with 69 digits just for fun :D");
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE...");
+            Console.ReadKey();
+            Console.Clear();
+            for (int i = 0; i < 69; i++)
+            {
+                a.Add(i);
+                Console.WriteLine(a);
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine("\nSeems like it works just fine!");
+        }
+    }
+}
---
+++ Lesson_2_main/aNDiQLib/MyList.cs
@@ -0,0 +1,166 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace aNDiQLib
+{
+    public class MyList<T>
+    {
+        private T[] _list = new T[4];
+        private int _count = 0;
+        private int _capacity = 4;
+
+        public MyList() 
+        {
+            _list = new T[4];
+            _count = 0;
+        }
+
+        public void Add(T item)
+        {
+            try
+            {
+                _list[_count++] = item;
+            }
+            catch (IndexOutOfRangeException)
+            {                
+                ResizeMyList();
+                _list[_count-1] = item;                
+            }
+        }
+
+        public override string ToString()
+        {
+            if (_count == 0) return "[]";
+            
+            string s = "[";
+            for (int i = 0; i < _count-1; i++)
+            {
+                s += _list[i] + ", ";
+            }            
+            return s + _list[_count-1] + "]";
+        }
+
+        /*
+         * In this example I use only x2, like in original
+         * But I added doubling down if we delete from MyList
+         */
+        private void ResizeMyList()
+        {
+            int capacity;
+            capacity = (_count <= _capacity / 2) ? Math.Max(_capacity / 2, 4) : _capacity * 2;
+
+            Console.WriteLine("\n!!!");
+            Console.WriteLine("MyArray was resized! That's because you're trying to Add or Remove an item!");
+            Console.WriteLine("It will have new capacity: " + _capacity + "->" + capacity);
+
+            _capacity = capacity;
+            T[] tempList = new T[capacity];
+            for (int i = 0; i < _list.Length; i++)
+            {
+                tempList[i] = _list[i];
+            }
+            _list = tempList;
+
+            Console.WriteLine("Count: " + _count);
+            Console.WriteLine("Capacity: " + _capacity);
+            Console.WriteLine("!!!\n");
+        }
+
+        public void Insert(T item, int index)
+        {
+            if (index > _count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            _count++;
+            if (_count >= _capacity)
+            {
+                ResizeMyList();
+            }
+
+            T[] tempList = new T[_capacity];
+            for (int i = 0; i < index; i++)
+            {
+                tempList[i] = _list[i];
+            }
+            tempList[index] = item;
+            for (int i = index + 1; i < _count; i++) 
+            {
+                tempList[i] = _list[i - 1];
+            }
+            _list = tempList;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);            
+            if (index < 0)
+            {
+                return false;
+            }
+            else
+            {
+                RemoveAt(index);
+                return true;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            _count--;
+
+            T[] tempList = new T[_count];
+            for (int i = 0; i < index; i++)
+            {
+                tempList[i] = _list[i];
+            }
+            for (int i = index; i < _count; i++)
+            {
+                tempList[i] = _list[i+1];
+            }
+            
+            _list = tempList;
+            
+            CheckResizeOptimization();
+        }
+
+        private void CheckResizeOptimization()
+        {
+            if (_count <= _capacity / 2)
+            {
+                ResizeMyList();
+            }
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+            _capacity = 4;
+            _list = new T[_capacity];
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) >= 0;
+        }
+
+        
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (item != null && item.Equals(_list[i]))
+                {                    
+                    return i;
+                }
+            }
+            return -1;
+        }
+        
+        public T this[int i]
+        {
+            get => _list[i];
+            set => _list[i] = value;
+        }
+    }
+}
